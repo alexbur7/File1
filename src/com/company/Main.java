2ffa77fcc90146b7423377013f4c3df738890dff
alexbur7 <burdelev.a.d@edu.mirea.ru>
2019-12-06 14:06:05 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+     Figure f= new Figure(3);
+     try {
+         System.out.println("Работаю с первой точкой");
+         f.setCoord(0,0,0);
+         System.out.println("Работаю со второй точкой");
+         f.setCoord(1,10,0);
+         System.out.println("Работаю с третьей точкой");
+         f.setCoord(2,10,10);
+         System.out.println("Работаю с четвертой точкой");
+         f.setCoord(3,0,10);
+         System.out.println("Всё");
+     } catch (PointIndexException e) {
+         System.out.println(e.getMessage()+": "+e.getIndex());
+     }
+    }
+}
---
+++ src/com/company/Point.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Point {
+    public double x,y;
+    public void setCoord(double x,double y){
+        this.x=x;
+        this.y=y;
+    }
+}
